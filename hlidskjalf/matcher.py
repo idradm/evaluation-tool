7c24c20db0c9d62e30820028708caa9fbe94b37d
Adam Robak <adamr@wikia-inc.com>
2014-05-29 17:59:26 +0200

    encoding changed

--- hlidskjalf/matcher.py
+++ hlidskjalf/matcher.py
@@ -7,9 +7,9 @@ class EpisodesMatcher():
         self.ok = Type.objects.get(name='OK')
 
     def match(self, result_item):
-        parsed_url = str(urllib.unquote(result_item.result.value).decode('utf8')).lower()
-        if (parsed_url.find(str(result_item.result.item.episodeitem.series).lower().replace(' ', '')) >= 0 and
-                parsed_url.find(str(result_item.result.item.episodeitem.name).lower().replace(' ', '_')) >= 0):
+        parsed_url = urllib.unquote(result_item.result.value).encode('utf-8').lower()
+        if (parsed_url.find(result_item.result.item.episodeitem.series.lower().replace(' ', '')) >= 0 and
+                parsed_url.find(result_item.result.item.episodeitem.name.lower().replace(' ', '_')) >= 0):
             result_item.result.type = self.ok
             result_item.result.save()
             return True
