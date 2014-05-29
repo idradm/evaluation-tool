6cbc8e5adde8c17bec5c306ad4b0cec969c20d0d
Adam Robak <adamr@wikia-inc.com>
2014-05-29 18:18:19 +0200

    additional case for domain

--- hlidskjalf/matcher.py
+++ hlidskjalf/matcher.py
@@ -9,8 +9,9 @@ class EpisodesMatcher():
 
     def match(self, result_item):
         parsed_url = urllib.unquote(result_item.result.value).lower()
-        if (parsed_url.find(result_item.result.item.episodeitem.series.lower().replace(' ', '')) >= 0 and
-                parsed_url.find(result_item.result.item.episodeitem.name.lower().replace(' ', '_')) >= 0):
+        if ((parsed_url.find(result_item.result.item.episodeitem.series.lower().replace(' ', '')) >= 0 or
+            parsed_url.find(result_item.result.item.episodeitem.series.lower().replace(' ', '-')) >= 0) and
+                (parsed_url.find(result_item.result.item.episodeitem.name.lower().replace(' ', '_')) >= 0)):
             result_item.result.type = self.ok
             result_item.result.save()
             return True
