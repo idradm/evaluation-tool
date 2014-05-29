d2e0d1ed20fe4e73646c4edb6c73810224277b56
Adam Robak <adamr@wikia-inc.com>
2014-05-29 18:12:40 +0200

    unicode fix

--- hlidskjalf/matcher.py
+++ hlidskjalf/matcher.py
@@ -1,4 +1,5 @@
 import urllib
+import codecs
 from hlidskjalf.models import Type
 
 
@@ -7,7 +8,7 @@ class EpisodesMatcher():
         self.ok = Type.objects.get(name='OK')
 
     def match(self, result_item):
-        parsed_url = urllib.unquote(result_item.result.value).encode('utf-8').lower()
+        parsed_url = urllib.unquote(result_item.result.value).lower()
         if (parsed_url.find(result_item.result.item.episodeitem.series.lower().replace(' ', '')) >= 0 and
                 parsed_url.find(result_item.result.item.episodeitem.name.lower().replace(' ', '_')) >= 0):
             result_item.result.type = self.ok
